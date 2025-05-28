efcc8d5354c73fcb3eaada0afc1bf0564d178363
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-05-29 01:06:39 +0500

    cron applied

---
+++ src/config/cron.config.js
@@ -0,0 +1,22 @@
+import cron from "cron";
+import https from "https";
+
+const job = new cron.CronJob("*/14****", function () {
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) console.log("GET request sent successfully");
+      else console.log("GET request failed", res.statusCode);
+    })
+    .on("error", (e) => console.error("Error sending GET request:", e));
+});
+
+export default job;
+
+//EXAMPLES && EXPLANAITION
+// *14 * * * * - Every 14 minutes
+// * * * * * - Every hour
+//* * * * * - Every minute
+//* * * * * * - Every second
+//* 0 0 * * * - Every day at midnight
+//* * * * * * * - Every millisecond
+//* * * * * * * * - Every microsecond
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { initDB } from "./config/db.config.js";
 import rateLimiter from "./middleware/rate-limiter.js";
 import { specs, swaggerUi } from "../swagger/swagger.js";
 import { router } from "./routes/transaction.route.js";
+import job from "./config/cron.config.js";
 
 const app = express();
 
@@ -13,6 +14,8 @@ const PORT = process?.env?.PORT || 5001;
 app.use(express.urlencoded({ extended: true }));
 app.use(rateLimiter);
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") job.start();
 
 app.use("/api", router);
 
